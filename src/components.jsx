18b5cf1f197c8fd5794838553cc86458d69f2096
sinan-894 <mohammedsinan1011@gmail.com>
2025-08-16 00:34:37 +0530

    Create personal information form
    >personal information component has been updated
    >a form is created in this component
    >a inputtext companent is crteated to return input fields
    with label
    >these input field is made controlled and the inputs are kept track
    >data state is created to store the full data
    >on submit of form the data is stored to the data state

--- src/components.jsx
+++ src/components.jsx
@@ -11,24 +11,54 @@ export default  function App(){
     })
     return(
         <>
-        <Input onSubmit={setData}></Input>
+        <Data onSubmit={setData} data={data}></Data>
         <Resume data={data}></Resume>
         </>
     )
 }
 
-function Input({onSubmit}){
+function Data({onSubmit,data}){
     return(
         <div className="main-input-div">
-            <PersonalInformation onSubmit={onSubmit}></PersonalInformation>
-            <Education onSubmit={onSubmit}></Education>
-            <WorkExperience onSubmit={onSubmit}></WorkExperience>
+            <PersonalInformation onSubmit={onSubmit} data={data}></PersonalInformation>
+            <Education onSubmit={onSubmit} data={data}></Education>
+            <WorkExperience onSubmit={onSubmit} data={data}></WorkExperience>
         </div>
     )
 }
 
 
-function PersonalInformation({onSubmit}){
+function PersonalInformation({onSubmit,data}){
+    const [fullName,setFullName] = useState("")
+    const [email,setEmail] = useState("")
+    const [contact,setContact] = useState("")
+    const [address,setAddress] = useState("")
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        onSubmit({...data,fullName,email,contact,address})
+
+    }
+
+    return(
+        <div className="personal-information">
+            <h1>Personal Information</h1>
+            <form action="#">
+                <InputText name={'full-name'} value={fullName} setInput={setFullName}>
+                    Full Name
+                </InputText>
+                <InputText name={'email'} value={email} setInput={setEmail}>
+                    email
+                </InputText>
+                <InputText name={'contact'} value={contact} setInput={setContact}>
+                    contact
+                </InputText>
+                <InputText name={'address'} value={address} setInput={setAddress}>    
+                    Address
+                </InputText>
+            </form>
+            <button onClick={handleSubmit}>submit</button>
+        </div>
+    )
 }
 
 function Education({onSubmit}){
@@ -37,6 +67,18 @@ function Education({onSubmit}){
 
 function WorkExperience({onSubmit}){
 
+}
+
+function InputText({name,children,setInput,value}){
+    const handleChange = (e)=>{
+        setInput(e.target.value)
+    }
+    return(
+        <>
+        <label htmlFor={name}>{children}</label>
+        <input type="text" name={name} value={value} id={name} onChange={handleChange}/>
+        </>
+    )
 }
 
 function Resume({data}){
